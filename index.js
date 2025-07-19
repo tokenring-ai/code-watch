6e2cc297cef0b4c6d66de4b0e8038128fd3a1922
Mark Dierolf <mdierolf@cloudquote.io>
2025-07-19 08:40:04 -0700

    Configured biome, and added it to the pre-commit checks

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 export { default as CodeWatchService } from "./CodeWatchService.js";
 
 export const name = "@token-ring/code-watch";
-export const description = "Service for watching code changes and triggering actions";
+export const description =
+	"Service for watching code changes and triggering actions";
 export const version = "0.1.0";
-
-
